3e1bf0aa352bb78377346f1125b00ca3f867aa54
Vamship113 <vinnup113@gmail.com>
2024-11-02 20:13:51 +0530

    added resources and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -29,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding=true
+    }
 }
 
 dependencies {
@@ -37,7 +41,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation("com.github.bumptech.glide:glide:4.13.0")
+    androidTestImplementation("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
 }
